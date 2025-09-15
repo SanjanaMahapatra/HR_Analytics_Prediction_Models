4c0bb19e592ddf7441f6ef84b93efd27fd7a4562
Sanjana Mahapatra <sanjanamahapatra46206@gmail.com>
2025-09-15 22:10:35 +0530

    data cleaning and preprocessing done for objective 3 by merging 3 different datasets

--- IBM_HR_Analytics_Dataset_Prediction_Models/kaggle_dataset_download.py
+++ IBM_HR_Analytics_Dataset_Prediction_Models/kaggle_dataset_download.py
@@ -3,4 +3,3 @@ import kaggle
 
 kaggle.api.authenticate()
 
-kaggle.api.dataset_download_files('pavansubhasht/ibm-hr-analytics-attrition-dataset', path='dataset/', unzip=True)
--- Objective_3/objective_3_dataset.py
+++ Objective_3/objective_3_dataset.py
@@ -10,7 +10,7 @@ kaggle.api.dataset_download_files('mfaisalqureshi/hr-analytics-and-job-predictio
 
 kaggle.api.dataset_download_files('adnananam/employee-attrition-data', path='dataset/', unzip=True)
 
-
+kaggle.api.dataset_download_files('pavansubhasht/ibm-hr-analytics-attrition-dataset', path='dataset/', unzip=True)
 
 
 
